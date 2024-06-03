aae5d0b7fdbe533d6a41febf7b3e7728b4aab8aa
Kristen Skruber <skruberk@gmail.com>
2024-06-02 18:52:51 -0700

    Add files via upload
    The x axis value range is now automatically calculated by determining the x value where the fitted y value crosses 0.1

--- fitexpontential.m
+++ fitexpontential.m
@@ -2,10 +2,70 @@ clc;    % Clear the command window.
 clearvars;
 close all;  % Close all figs
 
+%hits_boundarywrap_30_5.000000e-01_10000_1000_1
+%hits_boundarywrap_5_5.000000e-01_10000_1000_1
+%hits_boundarywrap_20_5.000000e-01_10000_1000_1
+%hits_boundarywrap_10_5.000000e-01_10000_1000_1
+%hits_boundarywrap_10_2_10000_1000_1
+%hits_boundarywrap_15_2_10000_1000_1
+%hits_boundarywrap_20_2_10000_1000_1
+%hits_boundarywrap_30_2_10000_1000_1
+%hits_boundarywrap_5_2_10000_1000_1
+%hits_boundarywrap_3_2_10000_1000_1
+%hits_boundarywrap_3_1_10000_1000_1
+%hits_boundarywrap_15_1_10000_1000_1
+%hits_boundarywrap_5_1_10000_1000_1
+%hits_boundarywrap_30_1_10000_1000_1
+%hits_boundarywrap_20_1_10000_1000_1
+%hits_boundarywrap_10_1_10000_1000_1
+%hits_boundarywrap_15_2_10000_1000_1
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%hits_boundarywrap_3_3.000000e-01_10000_1000_1
+%hits_boundarywrap_30_3.000000e-01_10000_1000_1
+%hits_boundarywrap_10_3.000000e-01_10000_1000_1
+%hits_boundarywrap_10_3.000000e-01_10000_1000_1.000000e-01
+%hits_boundarywrap_3_3.000000e-01_10000_1000_1.000000e-01
+%hits_boundarywrap_30_3.000000e-01_10000_1000_1.000000e-01
+%hits_boundarywrap_30_2_10000_1000_1.000000e-01
+%hits_boundarywrap_20_2_10000_1000_1.000000e-01
+%hits_boundarywrap_15_2_10000_1000_1.000000e-01
+%hits_boundarywrap_10_2_10000_1000_1.000000e-01
+%hits_boundarywrap_5_2_10000_1000_1.000000e-01
+%hits_boundarywrap_3_2_10000_1000_1.000000e-01
+%hits_boundarywrap_3_1_10000_1000_1.000000e-01
+%hits_boundarywrap_5_1_10000_1000_1.000000e-01
+%hits_boundarywrap_10_1_10000_1000_1.000000e-01
+%hits_boundarywrap_15_1_10000_1000_1.000000e-01
+%hits_boundarywrap_20_1_10000_1000_1.000000e-01
+%hits_boundarywrap_30_1_10000_1000_1.000000e-01
+%hits_boundarywrap_30_5.000000e-01_10000_1000_1.000000e-01
+%hits_boundarywrap_20_5.000000e-01_10000_1000_1.000000e-01
+%hits_boundarywrap_15_5.000000e-01_10000_1000_1.000000e-01
+%hits_boundarywrap_10_5.000000e-01_10000_1000_1.000000e-01
+%hits_boundarywrap_5_5.000000e-01_10000_1000_1.000000e-01
+%hits_boundarywrap_3_5.000000e-01_10000_1000_1.000000e-01
+%hits_boundarywrap_15_5.000000e-01_10000_1000_1
+%hits_boundarywrap_3_5.000000e-01_10000_1000_1
+
+
+%hits_boundarywrap_10_1.500000e+00_10000_1000_1
+%hits_boundarywrap_5_2.500000e+00_10000_1000_1
+%hits_boundarywrap_10_2.500000e+00_10000_1000_1
+%hits_boundarywrap_15_2.500000e+00_10000_1000_1
+%hits_boundarywrap_20_2.500000e+00_10000_1000_1
+%hits_boundarywrap_30_2.500000e+00_10000_1000_1
+%hits_boundarywrap_30_1.500000e+00_10000_1000_1
+%hits_boundarywrap_20_1.500000e+00_10000_1000_1
+%hits_boundarywrap_15_1.500000e+00_10000_1000_1
+%hits_boundarywrap_5_1.500000e+00_10000_1000_1
+%
+%
+%
 % import from csv
-filename = 'steps2hit_springtarget_20_1_1000_10000_1.000000e-01.csv';
+filename = 'hits_boundarywrap_5_1.500000e+00_10000_1000_1.csv';
 dataTable = readtable(filename, 'VariableNamingRule', 'preserve'); 
 printFile = strrep(filename, '_', ' ');
+
 
 % Read the column of CSV file
 data = dataTable{:, 3};
@@ -56,15 +116,40 @@ options = statset('Display', 'off', 'TolFun', 1e-6, 'TolX', 1e-6);
 
 %fit values
 fittedYData = expModel(beta, xData);
-
-
 %error check for Inf or NaN values in the fitted curve
 if any(~isfinite(fittedYData))
     disp('contains Inf or NaN values.');
 end
+
+% check where x is 0, where the fitted y values first fall below the threshold
+threshold = 2.1;
+belowThresholdIdx = find(fittedYData < threshold, 1);
+% Check if we found a point below the threshold
+if ~isempty(belowThresholdIdx)
+    % If the exact point is needed, perform linear interpolation to find a more precise x value
+    if belowThresholdIdx > 1
+        x1 = xData(belowThresholdIdx - 1);
+        x2 = xData(belowThresholdIdx);
+        y1 = fittedYData(belowThresholdIdx - 1);
+        y2 = fittedYData(belowThresholdIdx);
+        % Linear interpolation formula
+        xzero = x1 + (threshold - y1) * (x2 - x1) / (y2 - y1);
+    else
+    disp('The fitted y values do not cross zero within the range of xData.');
+    end
+end
 % define point range for slopes 
-xRange2 = [200, 1000];  % data range for slope
-xRange1 = [30, 200];
+xRange2 = [xzero,10000];  % data range for slope
+xRange1 = [0,xzero];
+
+%fit data break into two ranges
+Yfit1 = xData >= xRange1(1) & xData <= xRange1(2);
+Yfit2 = xData >= xRange2(1) & xData <= xRange2(2);
+xDataRange1 = xData(Yfit1);
+fittedYData1 = fittedYData(Yfit1);
+xDataRange2 = xData(Yfit2);
+fittedYData2 = fittedYData(Yfit2);
+
 % fit values 
 fittedValuesAtEndpoints1 = expModel(beta, xRange1);
 disp('Fitted values at endpoint1:');
@@ -82,21 +167,28 @@ expDx = @(b, x) b(1) * b(2) * exp(b(2) * x) + b(3) * b(4) * exp(b(4) * x);
 %xEval= [50, 500]; %evaluate at those points
 %Dx = expDx(beta, xEval);
 % Dx xRange1
-xValues1 = linspace(xRange1(1), xRange1(2), 100);
+xValues1 = linspace(xRange1(1), xRange1(2), 1000);
 dxValues1 = expDx(beta, xValues1);
 % Dx xRange2
-xValues2 = linspace(xRange2(1), xRange2(2), 100);
+xValues2 = linspace(xRange2(1), xRange2(2), 1000);
 dxValues2 = expDx(beta, xValues2);
 % ave Dx xRange1
 aveDx1 = mean(dxValues1);
 % ave Dx xRange2
 aveDx2 = mean(dxValues2);
 
-% plot histogram and fitted curve
+% Find where the fitted y values first hit zero or cross zero
+%zeroCrossingIdx = find(diff(sign(fittedYData)) ~= 0, 1);
+
+
+% plot histogram and fitted curve%
 figure;
 bar(binCenters, binCounts, 'FaceColor', [0.8, 0.8, 0.8], 'EdgeColor', 'none');
 hold on;
-plot(xData, fittedYData, 'r-', 'LineWidth', 2);
+%plot(xData, fittedYData, 'r-','Linewidth', 2)
+plot(xDataRange1, fittedYData1, 'b-', 'LineWidth', 2);
+hold on;
+plot(xDataRange2, fittedYData2, 'r-', 'LineWidth', 2 )
 xlabel('Steps to Hit');
 ylabel('Raw Counts');
 title(printFile);
@@ -120,17 +212,20 @@ disp(aveDx2);
 %%filehandling%%%%%%%%%%%%%%%%%
 
 % Open a file for writing
-fileID = fopen('OutputSlope.csv', 'a');%w for write a for append   
-% Check if the file is empty
-fileEmpty = fseek(fileID, 0, 'eof') == 0;
-% Write headers only if the file is empty
-fileInfo = dir('OutputSlope.csv');
+fileID = fopen('OutputSlope3.csv', 'a');%w for write a for append   
+% Check if the file is empty and write headers if it is
+fileInfo = dir('OutputSlope3.csv');
 if fileInfo.bytes == 0
     fprintf(fileID, 'filename,slope1,slope2,beta\n');
+else
+    % Move to the end of the file and add a newline if the file is not empty
+    fseek(fileID, 0, 'eof');
+    fprintf(fileID, '\n');
 end
 
 %fileID = fopen(filename, 'a');
 % Write data to the CSV file
+fprintf(fileID, '%s,%.4f,%.4f,%s\n', printFile, slope1, slope2, num2str(beta, '%.4f'));
 
-fprintf(fileID, '%s,%.4f,%.4f,%s\n', printFile, slope1, slope2, num2str(beta, '%.4f '));
+%fprintf(fileID, '%s,%.4f,%.4f,%s\n\n', printFile, slope1, slope2, num2str(beta, '%.4f '));
 fclose(fileID);
